32336c79b6aede7beef1d6929b631a53d141cee6
Simon Josefsson <simon@josefsson.org>
2024-04-24 15:08:10 +0200

    telnetd/telnetd.h: Don't include config.h in a header file.
    Reported by Collin Funk in
    https://lists.gnu.org/archive/html/bug-gnulib/2024-04/msg00398.html

--- telnetd/telnetd.h
+++ telnetd/telnetd.h
@@ -16,7 +16,6 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see `http://www.gnu.org/licenses/'. */
 
-#include <config.h>
 #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/stat.h>
