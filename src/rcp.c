9931420a77b95fa47593fbb71cb2041fd04adfd6
Collin Funk <collin.funk1@gmail.com>
2024-07-27 21:25:30 -0700

    rcp: Don't call va_start without calling va_end.
    * src/rcp.c (run_err): Move va_start call after the early return.

--- src/rcp.c
+++ src/rcp.c
@@ -1381,11 +1381,12 @@ run_err (const char *fmt, ...)
   static FILE *fp;
   va_list ap;
 
-  va_start (ap, fmt);
-
   ++errs;
   if (fp == NULL && !(fp = fdopen (rem, "w")))
     return;
+
+  va_start (ap, fmt);
+
   fprintf (fp, "%c", 0x01);
   fprintf (fp, "rcp: ");
   vfprintf (fp, fmt, ap);
