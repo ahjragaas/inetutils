27ef59f916ff26cc14fe63743d9b7a3dd49937cb
Junhyuk IM(S2W) and Seungyun Lee(S2W) <zeroboo@s2w.inc>
2026-07-13 18:14:47 +0200

    telnet: ignore oversized SLC suboptions (tiny change)
    Signed-off-by: Simon Josefsson <simon@josefsson.org>

--- telnet/telnet.c
+++ telnet/telnet.c
@@ -1481,6 +1481,9 @@ slc_start_reply (void)
 void
 slc_add_reply (unsigned int func, unsigned int flags, cc_t value)
 {
+  if ((size_t) (slc_reply + sizeof slc_reply - slc_replyp) < 8)
+    return;
+
   if ((*slc_replyp++ = func) == IAC)
     *slc_replyp++ = IAC;
   if ((*slc_replyp++ = flags) == IAC)
